792e99eac8d4e527e1f05c63da9e78731d74436d
Sapna Tiwari <sapna.seattle@gmail.com>
2019-10-20 21:02:19 -0800

    Homework

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+function Logo() {
+    return (
+            <div className="logo">
+                <img className="logo-position" src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Books-1-icon.png" />
+                <h1 className="logo-text logo-position"> Find Books </h1>
+            </div>
+    )
+}
+
+export default Logo;
